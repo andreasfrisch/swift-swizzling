60733e1d302e2401685328b1de6a7bdc91d99ea1
Andreas Frisch <afr@tch-osx.local>
2022-04-29 08:23:35 +0200

    initial commit

---
+++ BuenasTardes/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  BuenasTardes
+//
+//  Created by Andreas Frisch on 28/04/2022.
+//
+
+import UIKit
+
+class ViewController: SIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+}
+
